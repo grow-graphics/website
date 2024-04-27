71489f7f7594fdf08f9ea80b2489b72dbc6f8f3b
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2024-04-27 17:19:50 -0600

    adjust base to see if it works

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://learn.grow.graphics',
+	site: 'https://grow-graphics.github.io/website/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
