48b6db4deb3242056ee194b3e8d8e4a5d30e7b91
Quentin Quaadgras <email@quentinquaadgras.com>
2025-01-24 17:57:18 +1300

    fix title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,35 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://learn.grow.graphics/',
-	integrations: [
-		starlight({
-			title: 'Grow Graphics',
-			logo: {
-				src: './src/assets/gdgrowlogo.svg',
-			},
-			social: {
-				github: 'https://github.com/grow-graphics/gd',
-			},
-			sidebar: [
-				{
-					label: 'Documentation',
-					autogenerate: { directory: 'documentation' },
-					order: 1,
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-					order: 2,
-				},
-				{
-					label: 'License',
-					autogenerate: { directory: 'license' },
-					order: 3,
-				},
-			],
-		}),
-	],
+  site: "https://learn.graphics.gd/",
+  integrations: [
+    starlight({
+      title: "Graphics GD",
+      logo: {
+        src: "./src/assets/gdgrowlogo.svg",
+      },
+      social: {
+        github: "https://github.com/grow-graphics/gd",
+      },
+      sidebar: [
+        {
+          label: "Documentation",
+          autogenerate: { directory: "documentation" },
+          order: 1,
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+          order: 2,
+        },
+        {
+          label: "License",
+          autogenerate: { directory: "license" },
+          order: 3,
+        },
+      ],
+    }),
+  ],
 });
